469cb25eeff635c1e85b1bc9d13d891940cbfc21
Jar0T <jarotumula4@gmail.com>
2020-05-12 14:45:52 +0200

    Added Modify Schedule user control

---
+++ src/E-SkiLift/Windows/User Controls/ModifyLiftScheduleUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E_SkiLift.Windows.User_Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ModifyLiftScheduleUserControl.xaml
+    /// </summary>
+    public partial class ModifyLiftScheduleUserControl : UserControl
+    {
+        public ModifyLiftScheduleUserControl()
+        {
+            InitializeComponent();
+            this.startDate.SelectedDate = DateTime.Today;
+            this.endDate.SelectedDate = DateTime.Today.AddDays(28);
+        }
+
+        private void modifyScheduleButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("NOT YET IMPLEMENTED\nSchedule modified.");
+        }
+    }
+}
