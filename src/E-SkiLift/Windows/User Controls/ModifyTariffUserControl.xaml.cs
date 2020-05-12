aa85c692f6daefbd819b908597a8945d96473cad
Jar0T <jarotumula4@gmail.com>
2020-05-12 14:46:47 +0200

    Added Modify Tariff user controll

---
+++ src/E-SkiLift/Windows/User Controls/ModifyTariffUserControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E_SkiLift.Windows.User_Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ModifyTariffUserControl.xaml
+    /// </summary>
+    public partial class ModifyTariffUserControl : UserControl
+    {
+        public ModifyTariffUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void modifyTariffButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("NOT YET IMPLEMENTED\nTariff modified.");
+        }
+    }
+}
