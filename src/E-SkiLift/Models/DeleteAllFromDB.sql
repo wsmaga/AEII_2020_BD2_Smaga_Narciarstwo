2f80114cf277be07c062f56048bf2f598b5ee602
Jar0T <jarotumula4@gmail.com>
2020-05-28 10:31:58 +0200

    Added script to delete all data from database

---
+++ src/E-SkiLift/Models/DeleteAllFromDB.sql
@@ -0,0 +1,18 @@
+﻿DELETE FROM LiftTariffSet;
+GO
+DELETE FROM LiftUsageHistorySet;
+GO
+DELETE FROM SkiLiftScheduleSet;
+GO
+DELETE FROM SkiLiftSet;
+GO
+DELETE FROM TicketSet_PointTicket;
+GO
+DELETE FROM TicketSet_SkiPass;
+GO
+DELETE FROM TicketTariffSet;
+GO
+DELETE FROM TicketSet;
+GO
+DELETE FROM UserSet;
+GO
