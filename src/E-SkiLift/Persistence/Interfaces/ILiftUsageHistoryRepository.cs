c7a65fff07cce72d8993c424bf634b657a90bff6
Jakub Borek (jakubor642) <jakubor642@student.polsl.pl>
2020-05-28 12:22:25 +0200

    Added LiftUsageHistory and TicketTariff repositories

---
+++ src/E-SkiLift/Persistence/Interfaces/ILiftUsageHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using E_SkiLift.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_SkiLift.Persistence.Interfaces
+{
+    public interface ILiftUsageHistoryRepository : IRepository<LiftUsageHistory>
+    {
+        IEnumerable<LiftUsageHistory> GetLiftUsageHistory(int liftId);
+    }
+}
---
+++ src/E-SkiLift/Persistence/Interfaces/ITicketTariffRepository.cs
@@ -0,0 +1,19 @@
+﻿using E_SkiLift.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_SkiLift.Persistence.Interfaces
+{
+    public interface ITicketTariffRepository : IRepository<TicketTariff>
+    {
+
+        TicketTariff GetLatestTicketTariff();
+        IEnumerable<TicketTariff> GetTicketTariffHistory();
+
+        IEnumerable<TicketTariff> GetTicketTariffHistory(DateTime beginDate, DateTime endDate);
+
+    }
+}
