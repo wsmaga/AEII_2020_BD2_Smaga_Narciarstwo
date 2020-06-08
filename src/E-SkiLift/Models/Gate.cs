60e55b791bf795df1aa0d1f587d1f98ca45d630f
wsmaga <witosma861@student.polsl.pl>
2020-06-08 23:30:03 +0200

    Gate mockup functionality - Added Gate model for lift's gate methods - Added validating ticket functionality

---
+++ src/E-SkiLift/Models/Gate.cs
@@ -0,0 +1,33 @@
+﻿using E_SkiLift.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_SkiLift.Models
+{
+    class Gate
+    {
+        private readonly UnitOfWork uow = new UnitOfWork(new ERDContainer());
+
+        public bool Validate(int ticketID, int liftID)
+        {
+            Ticket ticket = uow.Tickets.Get(ticketID);
+            SkiLift lift = uow.SkiLifts.Get(liftID);
+
+            if(ticket == null || lift == null)
+                return false;
+            else
+            {
+                LiftUsageHistory luh = new LiftUsageHistory();
+                luh.Date = DateTime.Now;
+                luh.SkiLiftID = liftID;
+                luh.TicketID = ticketID;
+
+                uow.LiftUsageHistory.Add(luh);
+                return uow.Complete() == 1;
+            }
+        }
+    }
+}
--- src/E-SkiLift/Windows/GatePage.xaml.cs
+++ src/E-SkiLift/Windows/GatePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_SkiLift.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
@@ -22,6 +23,7 @@ namespace E_SkiLift.Windows
     public partial class GatePage : Page
     {
         private readonly MainWindow parentWindow;
+        private readonly Gate gate = new Gate();
 
         public GatePage(MainWindow parentWindow)
         {
@@ -31,7 +33,22 @@ namespace E_SkiLift.Windows
 
         private void validateButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Ticket validated.\nNOT YET IMPLEMENTED");
+            try
+            {
+                if(gate.Validate(int.Parse(ticketID.Text), int.Parse(liftID.Text)))
+                {
+                    MessageBox.Show("Ticket validated!");
+                }
+                else
+                {
+                    MessageBox.Show("Could not validate ticket. Ticket and/or lift doesn't exist or connection to the database has been lost.", "ERROR!",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show("Invalid ticket and/or lift ID!");
+            }
         }
 
         private void logoutButton_Click(object sender, RoutedEventArgs e)
