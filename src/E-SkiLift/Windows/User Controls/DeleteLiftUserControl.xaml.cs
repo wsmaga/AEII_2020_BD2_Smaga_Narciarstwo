914c3a6f9b79939e61fe5d3f35ec482e5422f657
Jar0T <jarotumula4@gmail.com>
2020-06-12 16:16:25 +0200

    Current lift list reloads after deleting lift

--- src/E-SkiLift/Windows/User Controls/DeleteLiftUserControl.xaml.cs
+++ src/E-SkiLift/Windows/User Controls/DeleteLiftUserControl.xaml.cs
@@ -37,7 +37,11 @@ namespace E_SkiLift.Windows.User_Controls
             if (liftId.HasValue)
                 result = LoggedAdmin.RemoveSkiLift(liftId.Value);
             if (result)
+            {
                 MessageBox.Show("Successfuly deleted lift.");
+                DeleteLiftModel dlm = new DeleteLiftModel();
+                DataContext = dlm;
+            }
             else
             {
                 string val = liftId?.ToString() ?? "null";
